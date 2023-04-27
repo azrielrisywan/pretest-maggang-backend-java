bb9dbfb7bbf048d06e2167788efe45938c8e2a94
Azriel Attaufiqi <azrielrisywan@gmail.com>
2023-04-27 11:08:58 +0700

    add User model

---
+++ src/main/java/com/test_backend/backend/entity/User.java
@@ -0,0 +1,14 @@
+package com.test_backend.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String nama;
+    private String telepon;
+}
