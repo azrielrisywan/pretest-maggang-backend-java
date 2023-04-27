7d3826fefb276bc3047f3c0b0e227b70fcf5f379
Azriel Attaufiqi <azrielrisywan@gmail.com>
2023-04-27 10:10:04 +0700

    change directory

--- src/main/java/com/test_backend/backend/BackendApplication.java
+++ src/main/java/com/test_backend/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.examplebootcamp.backend;
+package com.test_backend.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
