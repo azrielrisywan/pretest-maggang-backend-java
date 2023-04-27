0ae40afa6bfbf39e4417431a874ee3144769b3f6
Azriel Attaufiqi <azrielrisywan@gmail.com>
2023-04-27 10:04:16 +0700

    initiate project

---
+++ src/main/java/com/examplebootcamp/backend/BackendApplication.java
@@ -0,0 +1,22 @@
+package com.examplebootcamp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@RequestMapping(value = "/helloWorld", method = RequestMethod.GET)
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
